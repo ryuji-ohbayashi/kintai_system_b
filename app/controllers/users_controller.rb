5be10ad23a2db8791362d8867d7e6d3964908817
ryuji-ohbayashi <j.d.30.capricorne@gmail.com>
2019-08-28 23:28:00 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id]) # 5.2 3~5 
   end
+  
+  def new
+    @user = User.new # 書き換え 5.4 ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+  end
+  
+  def create # 5.5.2 11~18
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user # 5.5.5
+    else
+      render :new
+    end
+  end
+  
+  private # 5.5.3 20~24
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top' # ← 2.8 #
   get '/signup', to: 'users#new' # ← 3.5.2 2削除/ #
+  resources :users # ← 5.2 #
+  
 end
