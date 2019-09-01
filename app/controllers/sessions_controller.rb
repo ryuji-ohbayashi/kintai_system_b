5596fe6338625b78a6f0c3564bdd5e5d75493da0
ryuji-ohbayashi <j.d.30.capricorne@gmail.com>
2019-09-01 22:34:22 +0000

    Finish full-fledged-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase) # 追加 6.2.2 6~12
     if user && user.authenticate(params[:session][:password])
       log_in user # 追加 6.3.1 8~9
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user) # 追加 7.1.4 / 変更 7.3
       redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。' # 追加 6.2.3
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy # 追加 6.6 16~20
-    log_out
+    log_out if logged_in? # 追加 7.2
     flash[:success] = 'ログアウトしました'
     redirect_to root_url
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,16 +5,38 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # 永続的セッションを記憶します（Userモデルを参照）
+  def remember(user) # 追加 7.1.3 9~13
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 永続的セッションを破棄します
+  def forget(user) # 追加 7.1.4 16~20
+    user.forget # Userモデル参照
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   # セッションと@current_userを破棄します
   def log_out # 追加 6.6 9~12
+  forget(current_user) # 追加 7.1.4
     session.delete(:user_id)
     @current_user = nil
   end
   
+  # 一時的セッションにいるユーザーを返します。
   #現在ログイン中のユーザーがいる場合オブジェクトを返します。
-  def current_user #追加 6.3.2 9~13
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+  def current_user # 追加 6.3.2 9~13 / 変更 7.1.3 23~32
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
